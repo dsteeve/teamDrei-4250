fa3f5aecc5cd2f61a8b0cf047f9ad226c298ba50
Dina Steeve <dsteeve@knowledgefactor.com>
2015-11-22 10:06:49 -0700

    fix bashRun.sh to accept a dir name and work on windows

--- bashRun.sh
+++ bashRun.sh
@@ -1,14 +1,22 @@
-# Filename: trial.sh
+# Filename: bashRun.sh
 
+# this scripts assumes you are running on a Windows platform without mono
 ##############################################################################
 
+[ "$#" -eq 1 ] || die "1 the test directory name is required "
+
 echo "========================================================================"
-echo "                              output"
+echo "                              begin system test for " $1
 echo ""
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo ""
-CollisionDetectionSystem/bin/Debug/CollisionDetectionSystem.exe testdir=$DIR/CollisionDetectionSystem/SystemTesting/TestData/SystemTests/TestFiles/1
+
+CollisionDetectionSystem\bin\Debug\CollisionDetectionSystem.exe testdir=$DIR\CollisionDetectionSystem\SystemTesting\TestData\SystemTests\TestFiles\$1
+
 echo ""
-echo "                               end"
+echo "                               end system test for " $1
 echo "========================================================================"
+
 #EOF
