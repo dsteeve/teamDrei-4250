4ffd7f7771a5c48d7132d17aa8cf30099a4f13ae
Luke Fisher <lukewfisher@msn.com>
2015-11-22 20:01:36 -0700

    created the logger class. has a constructer that deletes the file if it is there and creates a new one. has a write method to write to that file. has a shutdown method to close the connection.

---
+++ CollisionDetectionSystem/Logger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CollisionDetectionSystem
+{
+	public class Logger
+	{
+		public StreamWriter toLog;
+		string path = @"c:\CollisionSystemLog.txt";
+
+		public Logger ()
+		{
+			if (File.Exists(path))
+			{
+				//removes the file if it exists 
+				File.Delete(path);
+			}	
+			//creates the file
+			FileStream.create (path);
+
+			try{
+				//attepts to create a stream writer that points at the file
+				toLog = new StreamWriter (path);
+			}
+			catch(Exception E){
+				throw;
+			}
+
+		}
+
+		//write method that takes a string and an event that has a message, the log creates a line that is the concadination of the two.
+		public void write(string line, BuildEventArgs message){
+			toLog.WriteLine (line + message.Message);
+		}
+
+		//used to close the connection to the file
+		public void shutDown(){
+			toLog.Close ();
+		}
+}
+
